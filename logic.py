284b066d1bb61cca81dc43550fb22369ef25896d
Parsoph85 <parsoph85@gmail.com>
2024-07-20 09:37:53 +0300

    Корректировка скриптов, наполнение БД

--- logic.py
+++ logic.py
@@ -9,8 +9,6 @@ class Events:
 
     def create(self, data: str):
         self.data = data
-        ext_mess_ttl = ''
-        ext_mess_txt = ''
         inp_str = data.split("|")
         data = inp_str[0]
         data_m = re.match(r'^[0-9]{4}-{1}[0-9]{2}-{1}[0-9]{2}$', inp_str[0])
@@ -26,10 +24,9 @@ class Events:
 
             db_string = f"SELECT data FROM events WHERE data = '{data}'"
             answer = db.db_work(db_str=db_string)
-            print("answer", answer)
             if answer == []:
                 db_string = f"INSERT INTO events (data, title, text) VALUES ('{data}','{title}','{text}')"
-                answer = db.db_work(db_str=db_string)
+                db.db_work(db_str=db_string)
                 return ("Success. ")
             else:
                 return ("Дата уже есть. ")
@@ -54,8 +51,6 @@ class Events:
     def update(self, n_id: int, data: str):
         self.data = data
         self.n_id = n_id
-        ext_mess_ttl = ''
-        ext_mess_txt = ''
         inp_str = data.split("|")
         data = inp_str[0]
         title = inp_str[1]
@@ -67,11 +62,11 @@ class Events:
         if len(text) > Note.text_len:
             text = text[0:Note.text_len]
         db_string = f"UPDATE events SET data = '{data}', title = '{title}', text = '{text}' WHERE id = '{n_id}'"
-        answer = db.db_work(db_str=db_string)
+        db.db_work(db_str=db_string)
         return ("Success. ")
 
     def delete(self, n_id: int):
         self.n_id = n_id
         db_string = f"DELETE FROM events WHERE id='{n_id}'"
-        answer = db.db_work(db_str=db_string)
+        db.db_work(db_str=db_string)
         return ("Success. ")
